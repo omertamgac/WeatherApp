3eefd7e892c7312e019ff20ac6d2bdf111af7c6f
Ömer Faruk Tamgaç <99759641+omertamgac@users.noreply.github.com>
2024-05-03 12:14:21 +0300

    Update WeatherAPI.kt

--- app/src/main/java/com/example/weatherapp/WeatherAPI.kt
+++ app/src/main/java/com/example/weatherapp/WeatherAPI.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Query
 
 interface WeatherAPI {
 
-    //https://api.openweathermap.org/data/2.5/
+    
 
     @GET("weather")
     suspend fun getData(@Query("q") city: String, @Query("appid") apiKey: String): Response<WeatherModel>
